c1593b8bc22310dfbe076264019a5ba68a62d9bc
Max <maxime.bourassa@mail.mcgill.ca>
2024-05-10 15:50:51 +0200

    can specify ip

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, send_file, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -22,4 +23,9 @@ def download_image(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    port = 8000
+    ip_addr = '127.0.0.1'
+    if len(sys.argv) == 2:
+        ip_addr = sys.argv[1]
+    
+    app.run(debug=True, port=port, host=ip_addr)
